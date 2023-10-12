409294349d731e89df3873e760737dc9c790d488
uyuyuy99 <uyuyuy99@gmail.com>
2023-10-12 14:03:50 -0700

    Initial commit

---
+++ src/client/java/me/uyuyuy99/fabrictest2/ExampleModClient.java
@@ -0,0 +1,10 @@
+package me.uyuyuy99.fabrictest2;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExampleModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
